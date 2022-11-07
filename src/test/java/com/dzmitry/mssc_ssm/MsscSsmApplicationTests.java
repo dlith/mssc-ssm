a8727ec262a9dd480f3934a5bbf32ea6e61266a3
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-07 11:58:31 +0100

    init

---
+++ src/test/java/com/dzmitry/mssc_ssm/MsscSsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzmitry.mssc_ssm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscSsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
